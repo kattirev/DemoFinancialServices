9cff4a30ae6438ac9d09748d21f58d6349bc82dc
kattirev <revanasidda081@gmail.com>
2017-03-30 17:30:07 +0530

    second commit

--- src/AddRecord.java
+++ src/AddRecord.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/// second commit
 
 @WebServlet("/AddRecord")
 public class AddRecord extends HttpServlet {
